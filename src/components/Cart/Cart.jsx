6391a65aac179344e6d6f98350a9a29874f96ebf
hasinurrahman1 <hasinur1703@gmail.com>
2023-03-28 05:48:11 +0600

    added cart component and calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
-    // const cart = props.cart; // option 1
-    // const {cart} = props;   // option 2
+// const cart = props.cart; // option 1
+// const {cart} = props;   // option 2
 
-    console.log(cart);
-    let total = 0;
-    for(const product of cart){
-        total = total + product.price;
+console.log(cart);
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+
+    const tax = totalPrice*7/100;
+
+    const grandTotal =totalPrice + totalShipping + tax;
+    
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h4>Grand Total: </h4>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
